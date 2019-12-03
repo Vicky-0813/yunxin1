d850b8adac647ed3139db4fcccb9555e786083f2
Vicky-0813 <351408405@qq.com>
2019-12-03 11:36:26 +0800

    picture

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
-    <i-grid>
-    <i-grid-item>
+    <i-grid i-class="no-border">
+    <i-grid-item i-class="no-border">
         <i-grid-icon>
-            <image src="static/images/user.png" />
+            <image src="/static/images/1.png" />
         </i-grid-icon>
-        <i-grid-label>Grid</i-grid-label>
+        <i-grid-label>推荐</i-grid-label>
     </i-grid-item>
-    <i-grid-item>Grid</i-grid-item>
-    <i-grid-item>Grid</i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/images/2.png" />
+        </i-grid-icon>
+        <i-grid-label>吃喝</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/images/4.png" />
+        </i-grid-icon>
+        <i-grid-label>玩乐</i-grid-label>
+    </i-grid-item>
   </i-grid>
   </div>
 </template>
@@ -31,4 +41,10 @@ export default {
 </script>
 
 <style scoped>
+div >>> .no-border {
+  border-width: 0pt;
+}
+div >>> .split {
+  margin-bottom: 10pt;
+}
 </style>
